f47b848caa38ff63369289176789cd0a67a31747
farman khan <farmankhan977@gmail.com>
2018-09-01 01:25:50 +0530

    First Commit Using Git

---
+++ firstapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import  path, include
+from . import views
+from django.urls import  path, include
+from . import  views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+
+]
